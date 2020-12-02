24c5da5669cacd7b8455889d5e18e7677f92d4f6
黄泽辉(WB20200925007) <huangzehui@gbicc.net>
2020-12-02 17:26:41 +0800

    客户端

---
+++ src/main/java/com/huiying/handler/HttpClientHandler.java
@@ -0,0 +1,18 @@
+package com.huiying.handler;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.HttpContent;
+
+public class HttpClientHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        if (msg instanceof HttpContent) {
+            HttpContent content = (HttpContent) msg;
+            ByteBuf buf = content.content();
+            System.out.println(buf.toString(io.netty.util.CharsetUtil.UTF_8));
+            buf.release();
+        }
+    }
+}
